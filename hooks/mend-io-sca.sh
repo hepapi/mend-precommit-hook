7d37e21fe90746a6d26570a01a51547120f92e5d
Oguzhan Yilmaz <oguzhan@kloia.com>
2023-08-17 17:31:28 +0300

    update the script

--- hooks/mend-io-sca.sh
+++ hooks/mend-io-sca.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+# check if the mend cli is installed
+# check if the mend cli auth is completed
 # run mend sca and reflect the exit code.
-# mend deps
-echo "hey"
+
+
+# Check if 'mend' exists in PATH
+if command -v mend >/dev/null 2>&1; then
+    # Mend exists
+    pwd 
+else
+    echo "Error: 'mend' CLI does not exist on this system."
+    echo "Please install 'mend' or check your PATH settings."
+    exit 1
+    # todo: have a link to installation page
+fi
+
+
+mend deps
